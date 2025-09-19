19b38c53822c55e859809c7717295dd06af6b8fe
Daniel <daniel.m.adr@hotmail.com>
2025-09-19 20:44:30 -0300

    Add management fee calculation endpoint and update .gitignore

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+from calculator import calculate_management_fee_per_shareholder
+
+class DailyData(BaseModel):
+    valor: float
+    quantidades: List[float]
+
+class InvestmentData(BaseModel):
+    taxa: float
+    cotas: List[DailyData]
+
+app = FastAPI()
+
+@app.post("/calcular-taxa-administrativa-por-cotista", response_model=List[float])
+def evaluate_fee_per_investor(data: InvestmentData):
+    
+    input_dict = data.model_dump()
+  
+    management_fee_result = calculate_management_fee_per_shareholder(input_dict)
+    
+    return management_fee_result
