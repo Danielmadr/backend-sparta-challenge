d44a9d1dc275d994c91e83c23416ff3b6543930d
Daniel <daniel.m.adr@hotmail.com>
2025-09-20 13:10:44 -0300

    Update test data files for endpoint tests and correct expected response lengths

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -10,14 +10,14 @@ def test_get_endpoint():
     assert response.json() == {"System": "Backend Sparta Challenge", "Version": "1.0.0"}
 
 def test_post_endpoint_small_data():
-    with open('data/small_test_data.json', 'r') as f:
+    with open('data/test_data_stakeholders_40_days_30.json', 'r') as f:
         small_payload = f.read()
     response = requests.post(f"{ENDPOINT}/calcular-taxa-administrativa-por-cotista", data=small_payload, headers={"Content-Type": "application/json"})
     assert response.status_code == 200
     assert len(response.json()) == 40  # should return exactly 40 values, one for each shareholder
 
 def test_post_endpoint_large_data():
-    with open('data/large_test_data.json', 'r') as f:
+    with open('data/test_data_stakeholders_100k_days_30.json', 'r') as f:
         large_payload = f.read()
 
     inicio = time.time()
@@ -26,10 +26,10 @@ def test_post_endpoint_large_data():
     print(f"Tempo de resposta: {fim - inicio} segundos")
 
     assert response.status_code == 200
-    assert len(response.json()) == 40000  #should return exactly 40000 values, one for each shareholder
+    assert len(response.json()) == 100000  # should return exactly 100000 values, one for each shareholder
 
 def test_post_endpoint_large_data_brute_force():
-    with open('data/large_test_data.json', 'r') as f:
+    with open('data/test_data_stakeholders_100k_days_30.json', 'r') as f:
         large_payload = f.read()
 
     inicio = time.time()
@@ -38,4 +38,4 @@ def test_post_endpoint_large_data_brute_force():
     print(f"Tempo de resposta força bruta: {fim - inicio} segundos")
 
     assert response.status_code == 200
-    assert len(response.json()) == 40000  # should return exactly 40000 values, one for each shareholder
+    assert len(response.json()) == 100000  # should return exactly 100000 values, one for each shareholder
